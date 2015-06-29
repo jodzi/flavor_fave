731f37d43af029d214a6b696d3a45a407b1a4b39
= <=>
2015-06-29 12:57:57 -0400

    final updated files for flavor fave

--- app_files/views.py
+++ app_files/views.py
@@ -21,8 +21,7 @@ vect_all = joblib.load('vect_all.pkl')
 svd_all = joblib.load('svd_all.pkl')
 with open('lshf_model.pkl', 'r') as lshf:
     lshf = pickle.load(lshf)
-with open('prices.pkl', 'r') as prices:
-    prices = pickle.load(prices)
+
 
 client = MongoClient()
 
@@ -43,11 +42,6 @@ for wine in wines:
     rounded = round(wine['review/points'], 2)
     wine['review/points'] = rounded
     
-#    if wine['wine/name'] in prices.keys():
-#        wine['price'] = prices[wine['wine/name']]
-#    else:
-#        wine['price'] = 'Not Available'
-    
     foods = []    
     for food in food_list:
         if food in wine['review/text']:
@@ -62,40 +56,42 @@ def index():
     with open('index.html', 'r') as home:
         return home.read()
         
-# Get an example and return its score
+
 @app.route('/wines', methods=["POST"])
 def find_wines():
 
     data = request.json
     
-    print data
-    
+    # Pull in keywords user selected (single or multiple)    
     flavors1 = data['descriptions']
-    flavors2 = data['pair_desc']
     
-#    if '[' in flavors1:
-#        key_words = ' '.join(flavors1.split(',')).lower()
-#    else:
-    key_words2 = ' '.join(flavors2).lower()
     key_words = re.findall('[A-Z][^A-Z]*', flavors1)
     key_words = ' '.join(key_words).lower()
+    
     flavors = []
     flavors.append(key_words)
-
+    
     vec_reviews = vect_all.transform(flavors)
     svd_reviews = svd_all.transform(vec_reviews)
     neighbors = lshf.kneighbors(svd_reviews)
     
-    vec_reviews2 = vect_all.transform(key_words2)
-    svd_reviews2 = svd_all.transform(vec_reviews2)
-    neighbors2 = lshf.kneighbors(svd_reviews2)
-
-    nearest_wines = []
-    nearest_pairings = []
+    nearest_wines = []    
     for i in neighbors[1][0]:
         nearest_wines.append(wines[i])
     nearest_wines = sorted(nearest_wines, key=itemgetter('review/points'), reverse=True)
     
+    # Pull in flavor pairings that are preset by me    
+    flavors2 = data['pair_desc']
+
+    key_words2 = ' '.join(flavors2).lower()
+    
+    pairings = []
+    pairings.append(key_words2)
+    vec_reviews2 = vect_all.transform(pairings)
+    svd_reviews2 = svd_all.transform(vec_reviews2)
+    neighbors2 = lshf.kneighbors(svd_reviews2)
+    
+    nearest_pairings = []
     for i in neighbors2[1][0]:
         nearest_pairings.append(wines[i])
     nearest_pairings = sorted(nearest_pairings, key=itemgetter('review/points'), reverse=True)
